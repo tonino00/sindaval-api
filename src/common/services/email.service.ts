4a9974e4c7d5d2a10660ffd8daec4cca461fc34c
tonino00 <antoninocgama@hotmail.com>
2026-03-29 16:06:19 -0300

    fix: corrigir importação do SendGrid para usar export default em vez de namespace import

--- src/common/services/email.service.ts
+++ src/common/services/email.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as sgMail from '@sendgrid/mail';
+import sgMail from '@sendgrid/mail';
 
 @Injectable()
 export class EmailService {
